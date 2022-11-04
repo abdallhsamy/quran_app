70d7a90d2d840ed150d1397c7ce54b7f536b0bff
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-11-05 01:31:29 +0200

    add translations to Ayah Service

--- src/ayah/services/ayah.service.ts
+++ src/ayah/services/ayah.service.ts
@@ -4,6 +4,7 @@ import {UpdateAyahDto} from "../dto/update-ayah.dto";
 import {AyahInterface} from "../interfaces/ayah.interface";
 import {Model} from "mongoose";
 import {CreateAyahDto} from "../dto/create-ayah.dto";
+import {I18n, I18nContext} from "nestjs-i18n";
 
 @Injectable()
 export class AyahService
@@ -17,40 +18,41 @@ export class AyahService
         return item.save();
     }
 
-    async updateOne(id: string, updateAyahDto: UpdateAyahDto): Promise<AyahInterface> {
+    async updateOne(id: string, updateAyahDto: UpdateAyahDto, @I18n() i18n: I18nContext): Promise<AyahInterface> {
         const existing = await this.AyahModel.findByIdAndUpdate(id, updateAyahDto, { new: true });
 
         if (!existing) {
-            throw new NotFoundException(`Ayah #${id} not found`);
+            throw new NotFoundException(await i18n.t('ayah.ayah_id_not_found', { args: {id : id}}));
         }
 
         return existing;
     }
 
-    async getAll(): Promise<AyahInterface[]> {
+    async getAll(@I18n() i18n: I18nContext): Promise<AyahInterface[]> {
         const item = await this.AyahModel.find();
         if (!item || item.length == 0) {
-            throw new NotFoundException('Ayahs data not found!');
+
+            throw new NotFoundException(await i18n.t('no_ayat_found'));
         }
 
         return item;
     }
 
-    async getOne(id: string): Promise<AyahInterface> {
+    async getOne(id: string, @I18n() i18n: I18nContext): Promise<AyahInterface> {
         const existingAyah = await this.AyahModel.findById(id).exec();
 
         if (!existingAyah) {
-            throw new NotFoundException(`Ayah #${id} not found`);
+            throw new NotFoundException(await i18n.t('ayah.ayah_id_not_found', { args: {id : id}}));
         }
 
         return existingAyah;
     }
 
-    async deleteOne(id: string): Promise<AyahInterface> {
+    async deleteOne(id: string, @I18n() i18n: I18nContext): Promise<AyahInterface> {
         const deleted = await this.AyahModel.findByIdAndDelete(id);
 
         if (!deleted) {
-            throw new NotFoundException(`Ayah #${id} not found`);
+            throw new NotFoundException(await i18n.t('ayah.ayah_id_not_found', { args: {id : id}}));
         }
 
         return deleted;
