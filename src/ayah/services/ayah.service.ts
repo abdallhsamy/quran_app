218bda9916985c22d128d9974d785e065132cc5a
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-11-05 16:46:31 +0200

    fix i18n error

--- src/ayah/services/ayah.service.ts
+++ src/ayah/services/ayah.service.ts
@@ -18,41 +18,41 @@ export class AyahService
         return item.save();
     }
 
-    async updateOne(id: string, updateAyahDto: UpdateAyahDto, @I18n() i18n: I18nContext): Promise<AyahInterface> {
+    async updateOne(id: string, updateAyahDto: UpdateAyahDto): Promise<AyahInterface> {
         const existing = await this.AyahModel.findByIdAndUpdate(id, updateAyahDto, { new: true });
 
         if (!existing) {
-            throw new NotFoundException(await i18n.t('ayah.ayah_id_not_found', { args: {id : id}}));
+            throw new NotFoundException(`Ayah #${id} not found`);
         }
 
         return existing;
     }
 
-    async getAll(@I18n() i18n: I18nContext): Promise<AyahInterface[]> {
+    async getAll(): Promise<AyahInterface[]> {
         const item = await this.AyahModel.find();
         if (!item || item.length == 0) {
 
-            throw new NotFoundException(await i18n.t('no_ayat_found'));
+            throw new NotFoundException("no Ayat found!");
         }
 
         return item;
     }
 
-    async getOne(id: string, @I18n() i18n: I18nContext): Promise<AyahInterface> {
+    async getOne(id: string): Promise<AyahInterface> {
         const existingAyah = await this.AyahModel.findById(id).exec();
 
         if (!existingAyah) {
-            throw new NotFoundException(await i18n.t('ayah.ayah_id_not_found', { args: {id : id}}));
+            throw new NotFoundException(`Ayah #${id} not found`);
         }
 
         return existingAyah;
     }
 
-    async deleteOne(id: string, @I18n() i18n: I18nContext): Promise<AyahInterface> {
+    async deleteOne(id: string): Promise<AyahInterface> {
         const deleted = await this.AyahModel.findByIdAndDelete(id);
 
         if (!deleted) {
-            throw new NotFoundException(await i18n.t('ayah.ayah_id_not_found', { args: {id : id}}));
+            throw new NotFoundException(`Ayah #${id} not found`);
         }
 
         return deleted;
